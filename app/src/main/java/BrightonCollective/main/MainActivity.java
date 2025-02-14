afbc44e47e6e635c055f30c4aae3bc5d8464241a
Adam Brows <aabrowsbrows@gmail.com>
2025-02-14 14:53:39 +0000

    Splash screen code

--- app/src/main/java/BrightonCollective/main/MainActivity.java
+++ app/src/main/java/BrightonCollective/main/MainActivity.java
@@ -1,6 +1,9 @@
 package BrightonCollective.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +12,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override                              // Splash activity for the welcome page when loading into app
+            public void run() {
+                Intent intentSplash = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intentSplash);
+                finish();
+            }
+        },1000);
+        Log.d(TAG, "onCreate: ");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.d(TAG, "onStart: ");
+    }
 
         //TODO splash screen for the app when first loading in
     }
-}
