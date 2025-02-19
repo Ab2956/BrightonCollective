a643329f56cd0574d893cf36dbf44135938238b3
jayda <J.Souza1@uni.brighton.ac.uk>
2025-02-19 12:51:08 +0000

    commit

--- app/src/main/java/BrightonCollective/main/LoginActivity.java
+++ app/src/main/java/BrightonCollective/main/LoginActivity.java
@@ -1,6 +1,12 @@
 package BrightonCollective.main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +21,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        Button button1 = findViewById(R.id.LoginButton);
+        EditText editTextName1 = findViewById(R.id.UserInputEmail);
+        EditText editTextName2 = findViewById(R.id.UserInputPassword);
+        final String key = "28747215";
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailInput = editTextName1.getText().toString();
+                String passwordInput = editTextName2.getText().toString();
+
+                if(!emailInput.isEmpty()) {
+                    try {
+                        String encryptedEmail = encrypt(emailInput, key);
+
+                    }
+                }
+            }
+
+        });
+
 
         }
 
