11d0b803e74e7f6c8f035f014b0e2e24f7e00d1a
Adam Brows <aabrowsbrows@gmail.com>
2025-03-12 22:38:04 +0000

    commit

--- app/src/main/java/BrightonCollective/main/HomePage.java
+++ app/src/main/java/BrightonCollective/main/HomePage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,15 +21,24 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         Button settingsBtn = findViewById(R.id.settingsBtn);        // Buttons
-        Button homeBtn = findViewById(R.id.homeBtn);
+        ImageButton homeBtn = findViewById(R.id.homeBtn);
         Button sellBtn = findViewById(R.id.sellBtn);
-        Button searchBtn = findViewById(R.id.searchBtn);
-        Button myAccountBtn = findViewById(R.id.myAccountBtn);
+        ImageButton searchBtn = findViewById(R.id.searchBtn);
+        ImageButton messageBtn = findViewById(R.id.messagesBtn);
+        ImageButton myAccountBtn = findViewById(R.id.myAccountBtn);
 
         settingsBtn.setOnClickListener(new View.OnClickListener() {     // Intent for the changing of the page when clicked
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        messageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MessageActivity.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/BrightonCollective/main/ProductPageActivity.java
+++ app/src/main/java/BrightonCollective/main/ProductPageActivity.java
@@ -34,13 +34,6 @@ public class ProductPageActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_product_page);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
