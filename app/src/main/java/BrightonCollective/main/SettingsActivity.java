6daee3bb9f6ba7480406878d057688bfe22e495a
Adam Brows <aabrowsbrows@gmail.com>
2025-05-14 09:36:45 +0100

    updates

--- app/src/main/java/BrightonCollective/main/SettingsActivity.java
+++ app/src/main/java/BrightonCollective/main/SettingsActivity.java
@@ -3,16 +3,21 @@ package BrightonCollective.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,38 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         ImageButton backBtn = findViewById(R.id.backButton);
 
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){        // if else statement to change to dark/light mode
+            setTheme(R.style.Theme_BrightonCollective);
+        }else {
+            setTheme(R.style.Base_Theme_BrightonCollective);
+        }
+
+        Switch aSwitch;
+        aSwitch = findViewById(R.id.switchButton);          // switch to configure the app appearance
+        textView = findViewById(R.id.viewMode);
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            aSwitch.setChecked(true);
+        }
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);    // change to dark mode with the dark mode text
+                    textView.setText("Dark Mode");
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);    // change to light mode with the light mode text
+                    textView.setText("Light Mode");
+                    reset();
+                }
+            }
+
+            private void reset() {
+                Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
